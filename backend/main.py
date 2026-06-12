5c49c9baef87a4dde3ab72146a227c1ebd8aa317
Adona Joseph <adonaj799@gmail.com>
2026-06-12 22:45:14 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "https://jan-epif1mpfe-adona-s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
